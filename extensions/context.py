4116edece462cae9deacb7486197717e83a99398
Christopher Arndt <chris@chrisarndt.de>
2024-11-13 19:17:26 +0100

    fix: generate CLAP plugin id in reversed domain notation
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- extensions/context.py
+++ extensions/context.py
@@ -7,4 +7,7 @@ class ContextUpdater(ContextHook):
 
     def hook(self, context):
         context["repo_name"] = context["_copier_conf"]["dst_path"].name
+        domain = context["domain"]
+        cname = context["plugin_cname"]
+        context["plugin_clap_id"] = ".".join(list(reversed(domain.split('.'))) + [cname]).lower()
         context["year"] = datetime.date.today().year
